179b20c8640c3d251829ae7e7310384380dea90f
orblancog <orblancog@gmail.com>
2017-01-11 17:29:03 +0100

    fccee 76

--- GenerateInrays.C
+++ GenerateInrays.C
@@ -21,7 +21,8 @@ int GenerateInrays (const char * k, int N) {
   //  gROOT->Reset();
   //  int N =10000;//number of particles to generate
   int debug=1; // if debug 1, print goes to stdout
-  int madxtrac=1;//track in MAD-X and PTC_MAD-X
+  int tracktable=1; // if tracktable 1, print goes to madx track table format
+  int madxtrac=1;//track in 1=MAD-X and 2=PTC_MAD-X
 
   // Energy distribution 
   // 0=Uniform -EnergySpread/2 to EnergySpread/2
@@ -37,6 +38,7 @@ int GenerateInrays (const char * k, int N) {
   double_t betar  = 1;
 
   //Beam geometrical emittances
+  double_t ebeam = 1;
   double_t ex = 1;
   double_t ey = 1;
   double_t et = 1;
@@ -83,6 +85,7 @@ int GenerateInrays (const char * k, int N) {
   double_t upd=0;
 
   ofstream mydebug;
+  ofstream mytracktable;
   ofstream mymadxtrac;
   ifstream beta0in;
   string beta0string;
@@ -107,7 +110,8 @@ int GenerateInrays (const char * k, int N) {
   if (beta0in == 0) {
     // if we cannot open the file, 
     // print an error message and return immediatly
-   printf("Error: cannot open betaXXXX.txt!\n");
+    cout << "Error: cannot open "<<betafl->Data()<<" !"<<endl;
+    //   printf("Error: cannot open betaXXXX.txt!\n");
     return 1;
   }
   cout << "  ... reading file "<<betafl->Data()<<" (twiss params at input)"<<endl;
@@ -147,6 +151,12 @@ int GenerateInrays (const char * k, int N) {
     beam0in >> madx01;// >> madx02 >> madx03 >> madx04;
     //    cout << madx01<<endl;
     //    cout << madx01<<endl;
+    if (!strcmp(madx01,"energy")){
+      beam0in >> madx02;
+      beam0in >> madx03;
+      beam0in >> madx04;
+      cout<<"    "<<madx01<<" "<<madx04<<endl;
+      ebeam=atof(madx04);}
     if (!strcmp(madx01,"ex")){
       beam0in >> madx02;
       beam0in >> madx03;
@@ -183,6 +193,7 @@ int GenerateInrays (const char * k, int N) {
   beam0in.close();
 
   if (debug) mydebug.open ("debug.txt");
+  if (tracktable) mytracktable.open ("trackTABLE");
   if (madxtrac) mymadxtrac.open ("madxInrays.madx");
   mymadxtrac << "! GenerateInrays. orblancog. 2016.07.28\n";  
   mymadxtrac << "! Dummy file generated in root\n";
@@ -196,21 +207,28 @@ int GenerateInrays (const char * k, int N) {
   //  cout << sigmax0 << sigmay0;
   //  sigmas0  = TMath::Sqrt(et*0);//already assigned when reading beam0
   sigmad0  = 1.0/betar*Energyspread;//TMath::Sqrt(et*0);//\Delta E/(Pc) = 1/\beta_r * \Delta E/E
+  Double_t xbetarnd,pxbetarnd;
+  Double_t ybetarnd,pybetarnd;
+  Double_t tbetarnd,ptbetarnd;
 
   while (i<N){
     //x
-    xbeta = xrnd->Gaus(0,sigmax0);
-    pxbeta = pxrnd->Gaus(0,sigmapx0);
+    xbetarnd   =  xrnd->Gaus(0,1);
+    pxbetarnd  =  pxrnd->Gaus(0,1);
+    xbeta      =  TMath::Sqrt(ex*betax)*xbetarnd;
+    pxbeta     =  TMath::Sqrt(ex/betax)*pxbetarnd - alfax*TMath::Sqrt(ex/betax)*xbetarnd;
     xgausvalue = (gammax*xbeta*xbeta+2*alfax*xbeta*pxbeta+betax*pxbeta*pxbeta)/(TMath::TwoPi()/2*ex);
     //y
-    ybeta = yrnd->Gaus(0,sigmay0);
-    pybeta = pyrnd->Gaus(0,sigmapy0);
+    ybetarnd   = yrnd->Gaus(0,1);
+    pybetarnd  = pyrnd->Gaus(0,1);
+    ybeta      =  TMath::Sqrt(ey*betay)*ybetarnd;
+    pybeta     =  TMath::Sqrt(ey/betay)*pybetarnd - alfay*TMath::Sqrt(ey/betay)*ybetarnd;    
     ygausvalue = (gammay*ybeta*ybeta+2*alfay*ybeta*pybeta+betay*pybeta*pybeta)/(TMath::TwoPi()/2*ey);
     //d
     ups = bunchlrnd->Gaus(0,sigmas0);
     if (Edistr){
       upd = Espreadrnd->Gaus(0,sigmad0);
-      tgausvalue = upd*upd/(sigmad0*sigmad0)+ups*ups/(sigmas0*sigmas0);
+      tgausvalue = upd*upd/(sigmad0*sigmad0)/(TMath::TwoPi()/2) + ups*ups/(sigmas0*sigmas0)/(TMath::TwoPi()/2);
     }else{
       upd = Espreadrnd->Uniform(-0.5*sigmad0,0.5*sigmad0);//\Delta E/(Pc) = 1/\beta_r * \Delta E/E
       tgausvalue = ups*ups/(sigmas0*sigmas0);
@@ -218,14 +236,19 @@ int GenerateInrays (const char * k, int N) {
       
     if (xgausvalue<gauslimit && ygausvalue<gauslimit && tgausvalue<gauslimit){
       i++;
-      ux = xbeta + etax*Esprd;
-      upx = pxbeta + etapx*Esprd;
-      uy = ybeta + etay*Esprd;
-      upy = pybeta + etapy*Esprd;
+      ux  = xbeta  + etax *upd   + 0e-3;
+      upx = pxbeta + etapx*upd   - 0e-3;
+      uy  = ybeta  + etay *upd   + 0e-3;
+      upy = pybeta + etapy*upd   + 0e-3;
+      upd = upd                  - 0e-3;
       if (debug) mydebug <<ux<<'\t'<<upx<<'\t'<<uy<<'\t'<<upy<<"\t"<<ups<<"\t"<<upd<<endl ;
-      if (madxtrac) mymadxtrac << "start, x="<<ux<<",px="<<upx<<",y="<<uy<<",py="<<upy<<",t="<<ups<<",pt="<<upd<<";\n";
+      if (madxtrac==1) mymadxtrac << "start, x="<<ux<<",px="<<upx<<",y="<<uy<<",py="<<upy<<",t="<<ups<<",pt="<<upd<<";\n";
+      if (madxtrac==2) mymadxtrac << "ptc_start, x="<<ux<<",px="<<upx<<",y="<<uy<<",py="<<upy<<",t="<<ups<<",pt="<<upd<<";\n";
+      //      if (madxtrac==2) mymadxtrac << "ptc_start, x="<<ux<<",px="<<upx<<",y="<<uy<<",py="<<upy<<",t="<<ups<<",pt="<<"0"<<";\n";
+      if (tracktable==1) mytracktable <<' '<<i<<" 0 "<<ux<<' '<<upx<<' '<<uy<<' '<<upy<<' '<<ups<<' '<<upd<<" 0 "<<ebeam<<endl;//NUMBER,TURN,X,PX,Y,PY,T,PT,S,E
     }
   }
+  if (tracktable) mytracktable.close();
   if (debug) mydebug.close();
   mymadxtrac.close();
   
